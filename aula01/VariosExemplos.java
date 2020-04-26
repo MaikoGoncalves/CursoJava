e59992af5ddaaefe096e0ad9457f523f40bc4a02
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2020-04-25 23:59:31 -0300

    Dia 25042020
    aulas

---
+++ aula01/VariosExemplos.java
@@ -0,0 +1,13 @@
+package com.maiko.cursojava.aula01;
+
+public class VariosExemplos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+99682-0913
---
+++ aula13/OperadoresAritimeticos.java
@@ -0,0 +1,49 @@
+package com.maiko.cursojava.aula13;
+
+public class OperadoresAritimeticos {
+
+  public static void main(String[] args) {
+    int resultado = 1 +2;
+    System.out.println(resultado); //3
+    
+    resultado = resultado - 1;
+    System.out.println(resultado); //2
+    
+    resultado = resultado * 2;
+    System.out.println(resultado); //4
+    
+	resultado = resultado / 2;
+	System.out.println(resultado); //2
+	
+	resultado = resultado + 8;
+	System.out.println(resultado); //10
+	
+	resultado = resultado % 7;
+	System.out.println(resultado); //3
+
+	String primeiroNome = "Esta é ";
+	String segundoNome = "uma String concatenada";
+	String terceiroNome = primeiroNome + segundoNome;		
+	System.out.println(terceiroNome); // Esta é uma String concatenada
+	    
+	resultado = resultado + 1;
+	System.out.println(resultado); // 4
+	resultado = 4 + 6;
+	System.out.println(resultado); // 10 
+		
+	// Agora vamos aprender outras somas
+	
+	resultado ++;                  // Soma e mostra na tela 11
+	System.out.println(resultado); // 10 + 1 = 11 
+	
+	System.out.println(resultado++); //  Mostra na tela pra depois somar 11 
+	System.out.println(resultado);   // 12
+	
+	System.out.println(++resultado); // Soma antes e depois mostra na tela 13
+	resultado +=5;                   // Soma com qual quer numero
+	System.out.println(resultado);   // Imprime o resultado 18
+	//
+  
+  }
+
+}
