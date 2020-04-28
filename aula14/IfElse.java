57a60f8577714f584554c86aaa4a4b43be589cad
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2020-04-28 19:11:15 -0300

    Create IfElse.java
    Aulas dia 27/04/2020

---
+++ aula14/IfElse.java
@@ -0,0 +1,32 @@
+package com.maiko.cursojava.aula14;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	
+	/*System.out.print("Digite sua idade : ");
+	int idade = scan.nextInt();
+	
+	if (idade >= 18) {
+	   System.out.print("É maior de idade. Pode continuar");
+	                 }
+	else { 
+		System.out.print("Não é maior de idade. Opa não continua");
+	     }
+	*/
+    // barato <= 10
+	// 10 < valor < 15 - pedir desconto
+	// 15 <= valor 17  - pesquisar mais
+	// >= 17 muito caro
+	
+	System.out.println();
+	System.out.println("Entre com o preço do item : ");
+	double valor = scan.nextDouble();
+	
+	
+	}
+	}
+
